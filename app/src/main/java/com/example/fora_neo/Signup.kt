8cf719f27603e928fdf5fe0788a2016e90536206
josar <josarroliga0943@outlook.es>
2022-11-07 15:56:36 -0600

    Registro usuario implementado

--- app/src/main/java/com/example/fora_neo/Signup.kt
+++ app/src/main/java/com/example/fora_neo/Signup.kt
@@ -1,6 +1,6 @@
 package com.example.fora_neo
 
-import android.nfc.Tag
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,19 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
-import com.example.fora_neo.databinding.FragmentLoginBinding
+import com.example.fora_neo.databinding.FragmentSignupBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.auth.FirebaseUser
 
 
+class Signup : Fragment() {
 
-class LoginFragment : Fragment() {
-    private  lateinit var binding: FragmentLoginBinding
-
+    private lateinit var binding: FragmentSignupBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,18 +29,21 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        binding = FragmentSignupBinding.inflate(inflater, container, false)
 
         auth = Firebase.auth
 
-        binding.button.setOnClickListener (View.OnClickListener {
+        binding.btnSignup.setOnClickListener (View.OnClickListener {
             val email: String = binding.etUser.text.toString().trim() { it <= ' ' }
             val pw: String = binding.etPw.text.toString().trim() { it <= ' ' }
+            //val confirm_pw: String
 
-            if (validate (email, pw)){
-                auth.signInWithEmailAndPassword(email, pw).addOnCompleteListener(requireActivity()){ task ->
+            if (validate(email, pw)){
+                auth.createUserWithEmailAndPassword(email, pw).addOnCompleteListener(requireActivity()){ task ->
                     if (task.isSuccessful) {
-                        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                        findNavController().navigate(R.id.action_signup_to_loginFragment)
+
                     } else {
                         Log.d("Tag", "Error", task.exception)
                     }
@@ -54,18 +54,10 @@ class LoginFragment : Fragment() {
             }
         })
 
-        binding.tvSignup.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_signup)
-        }
-
         return binding.root
     }
 
     fun validate(email: String?, pw: String?): Boolean {
         return ((email != null) && (pw != null)) && (pw.length >= 5) && (email.contains("@"))
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 }
