97aab999ca212b776388eead0e03542cfa498747
allan.silva13589@est.uca.edu.ni <alexander.xampos10@gmail.com>
2022-11-06 22:59:42 -0600

    Creación del sidebar

--- app/src/main/java/com/example/fora_neo/HomeFragment.kt
+++ app/src/main/java/com/example/fora_neo/HomeFragment.kt
@@ -5,26 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.fora_neo.databinding.ActivityMainBinding
 import com.example.fora_neo.databinding.FragmentHomeBinding
+import com.google.android.material.navigation.NavigationView
 
 
 class HomeFragment : Fragment() {
-private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+
+        toggle = ActionBarDrawerToggle(activity, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding  = FragmentHomeBinding.inflate(layoutInflater)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
         binding.searchView.setOnClickListener {
-            it.findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
-        }
+            it.findNavController().navigate(R.id.action_homeFragment_to_searchFragment) }
     }
 
 
