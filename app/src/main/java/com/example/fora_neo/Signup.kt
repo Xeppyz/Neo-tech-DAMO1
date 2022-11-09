1c37b473292e2d4c0d0770d029a6eadb95239208
Marcelo <riverasoto.marcelo@gmail.com>
2022-11-09 00:37:26 -0600

    sign up

--- app/src/main/java/com/example/fora_neo/Signup.kt
+++ app/src/main/java/com/example/fora_neo/Signup.kt
@@ -1,6 +1,9 @@
 package com.example.fora_neo
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.net.wifi.hotspot2.pps.Credential
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,15 +13,23 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.fora_neo.databinding.FragmentSignupBinding
+import com.firebase.ui.auth.data.model.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
+import org.json.JSONObject
 
 
 class Signup : Fragment() {
 
     private lateinit var binding: FragmentSignupBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var firestore:FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,26 +43,62 @@ class Signup : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSignupBinding.inflate(inflater, container, false)
 
-        auth = Firebase.auth
+        auth = FirebaseAuth.getInstance()
+        firestore = Firebase.firestore
+
+
 
         binding.btnSignup.setOnClickListener (View.OnClickListener {
             val email: String = binding.etCorreo.text.toString().trim() { it <= ' ' }
             val pw: String = binding.etPw.text.toString().trim() { it <= ' ' }
+            val name:String = binding.etNombre.toString().trim() {it <= ' '}
+
+
             //val confirm_pw: String
 
             if (validate(email, pw)){
-                auth.createUserWithEmailAndPassword(email, pw).addOnCompleteListener(requireActivity()){ task ->
-                    if (task.isSuccessful) {
-                        findNavController().navigate(R.id.action_signup_to_loginFragment)
+                 auth.createUserWithEmailAndPassword(email, pw)
+                     .addOnSuccessListener {
+                         val profile = UserProfileChangeRequest.Builder()
+                             .setDisplayName(name)
+                             .build()
+                         it.user!!.updateProfile(profile)
+                             .addOnSuccessListener {
+                                 AlertDialog.Builder(activity).apply {
+                                     setTitle("Cuenta creada")
+                                     setMessage("Cuenta creada satisfactoriamente")
+                                     setPositiveButton("Aceptar"){ _: DialogInterface, _: Int ->
+                                         findNavController().navigate(R.id.action_signup_to_loginFragment)
+                                     }
+                                 }.show()
+                             }
+                             .addOnFailureListener{
+                                 AlertDialog.Builder(activity).apply {
+                                     setTitle("Error")
+                                     setMessage(it.message)
+                                 }.show()
+                             }
 
-                    } else {
-                        Log.d("Tag", "Error", task.exception)
-                    }
+
+                     }
+                     .addOnFailureListener{
+                         AlertDialog.Builder(activity).apply {
+                             setTitle("Error")
+                             setMessage(it.message)
+                         }.show()
+
                 }
+
+
+
+
+
             }
             else{
                 Toast.makeText(activity, "Algunos campos pueden estar erroneos. Vuelva a intentarlo.", Toast.LENGTH_LONG).show()
             }
+
+
         })
 
         return binding.root
@@ -60,4 +107,5 @@ class Signup : Fragment() {
     fun validate(email: String?, pw: String?): Boolean {
         return ((email != null) && (pw != null)) && (pw.length >= 5) && (email.contains("@"))
     }
+
 }
