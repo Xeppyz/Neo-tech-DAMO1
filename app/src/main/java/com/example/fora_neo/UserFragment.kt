1747e7d928c2524bf410210df0b1da9430e544ff
josar <josarroliga0943@outlook.es>
2022-11-13 01:38:57 -0600

    Configuraciones usuario implementadas

--- app/src/main/java/com/example/fora_neo/UserFragment.kt
+++ app/src/main/java/com/example/fora_neo/UserFragment.kt
@@ -12,11 +12,13 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.fora_neo.databinding.FragmentUserBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class UserFragment : Fragment() {
 
     private lateinit var userBinding: FragmentUserBinding
-    lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var toggle: ActionBarDrawerToggle
+    var auth = FirebaseAuth.getInstance()
 
 
     override fun onCreateView(
@@ -42,13 +44,40 @@ class UserFragment : Fragment() {
             true
         }
 
+        with(userBinding){
+            tvNombreUsuario.text = auth.currentUser!!.displayName
+            btnBack.setOnClickListener {
+                it.findNavController().navigate(R.id.action_userFragment_to_homeFragment)
+            }
 
+            btnSettings.setOnClickListener{
+                if (layoutSetting.visibility == View.VISIBLE){
+                    layoutSetting.visibility = View.GONE
+                }
+                else{
+                    layoutSetting.visibility = View.VISIBLE
+                }
+            }
 
-
-        userBinding.btnBack.setOnClickListener {
-            it.findNavController().navigate(R.id.action_userFragment_to_homeFragment)
+            btnGuardar.setOnClickListener {
+                validar()
+            }
         }
 
+    }
+
+    private fun validar(){
+        with(userBinding){
+            if (etCorreo.text.isBlank() && etPw.text.isBlank() && etPwConfirm.text.isBlank()){
+                Toast.makeText(activity, "No hay ningun dato ingresado", Toast.LENGTH_SHORT).show()
+            }
+            else if (etCorreo.text.isBlank() && etPw.text.isNotBlank() && etPwConfirm.text.isNotBlank()){
+                Toast.makeText(activity, "Contrasena guardada", Toast.LENGTH_SHORT).show()
+            }
+            else if (etCorreo.text.isNotBlank() && etPw.text.isBlank() && etPwConfirm.text.isBlank()){
+                Toast.makeText(activity, "Correo guardado", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 
