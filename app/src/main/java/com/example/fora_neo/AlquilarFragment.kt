dbfab320bd8f9c3bba229e9ada821f1966238149
Marcelo <riverasoto.marcelo@gmail.com>
2022-11-15 00:34:00 -0600

    implementacion subir alquiler

--- app/src/main/java/com/example/fora_neo/AlquilarFragment.kt
+++ app/src/main/java/com/example/fora_neo/AlquilarFragment.kt
@@ -7,14 +7,20 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import com.example.fora_neo.databinding.FragmentAlquilarBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import datos.Apartamento
 
 
 class AlquilarFragment : Fragment() {
     private lateinit var binding: FragmentAlquilarBinding
     lateinit var toggle: ActionBarDrawerToggle
+    val auth = FirebaseAuth.getInstance()
+    val db = FirebaseFirestore.getInstance()
     lateinit var userFragment: UserFragment
 
 
@@ -47,8 +53,38 @@ class AlquilarFragment : Fragment() {
             }
             true
         }
+        binding.btnConfirmarDatos.setOnClickListener {
+            val nomApartamento = binding.etNombreApartamento.text.toString()
+            val direccion = binding.etDireccionApartamento.text.toString()
+            val precio = binding.etPrecioApartamento.text.toString().trim() { it <= ' ' }
+            val nombreUs = auth.currentUser!!.displayName
+
+            val apartamento = Apartamento(nomApartamento, direccion, precio, nombreUs)
+
+            db.collection("apartamentos").add(apartamento)
+                .addOnSuccessListener {
+
+                 }
+                .addOnFailureListener{
+                    android.app.AlertDialog.Builder(activity).apply {
+                        setTitle("Error")
+                        setMessage(it.message)
+                    }.show()
+                }
+            limpiar()
+
+        }
 
     }
+    private fun limpiar(){
+        with(binding){
+            etPrecioApartamento.setText(" ")
+            etNombreApartamento.setText(" ")
+            etDireccionApartamento.setText(" ")
+            etNombreApartamento.requestFocus()
+        }
+    }
+
 
     private fun fragmentHome() {
         findNavController().navigate(R.id.action_alquilarFragment_to_homeFragment)
